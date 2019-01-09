c236905a6ec884c94d8aa8e0da21fba71f1b144b
codecademy <ccuser@codecademy.com>
2019-01-09 21:52:12 +0000

    Added test objects

---
+++ script.py
@@ -0,0 +1,2 @@
+desintations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "So Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
