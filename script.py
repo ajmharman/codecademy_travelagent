ad6e1e4e3108c0caf3ae6863b0bf3ea61d109bda
codecademy <ccuser@codecademy.com>
2019-01-10 19:15:33 +0000

    Added interest finder logic

--- script.py
+++ script.py
@@ -24,7 +24,37 @@ def add_attraction(destination, attraction):
 
 
 add_attraction('Los Angeles, USA', ['Venice Beach', 'beach'])
-print(attractions)
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("So Paulo, Brazil", ["So Paulo Zoo", ["zoo"]])
+add_attraction("So Paulo, Brazil", ["Ptio do Colgio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  
+  for attraction in attractions_in_city:
+    possible_attraction = attraction
+    attraction_tags = attraction[1]
+    
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+  return attractions_with_interest
+
+la_arts = find_attractions('Los Angeles, USA', ['art'])
+print(la_arts)
+    
+  
 
 
   
